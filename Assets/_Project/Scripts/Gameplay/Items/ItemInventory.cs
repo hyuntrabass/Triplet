7dd6f535b8c33d4c7d9b884457b4b44cb4e2bf42
Vincent ryu <bigasiler@gmail.com>
2026-09-13 16:30:23 +0900

    ItemInventory 추가

---
+++ Assets/_Project/Scripts/Gameplay/Items/ItemInventory.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+public class ItemInventory
+{
+    private readonly List<ItemState> _states = new();
+
+    public IReadOnlyList<ItemState> State => _states;
+    
+    public ItemInventory(IReadOnlyList<ItemDefinition> loadout)
+    {
+        if (loadout == null)
+        {
+            throw new ArgumentNullException(nameof(loadout));
+        }
+
+        if (loadout.Count == 0)
+        {
+            throw new ArgumentException("아이템 구성은 하나 이상이어야 합니다.", nameof(loadout));
+        }
+
+        foreach (var definition in loadout)
+        {
+            if (definition == null)
+            {
+                throw new ArgumentNullException("아이템 구성에 비어 있는 항목이 있습니다.", nameof(definition));
+            }
+
+            _states.Add(new ItemState(definition));
+        }
+    }
+
+    public ItemState GrantRandomItem()
+    {
+        int index = UnityEngine.Random.Range(0, _states.Count);
+        ItemState grantedState = _states[index];
+
+        grantedState.Add();
+
+        return grantedState;
+    }
+}
