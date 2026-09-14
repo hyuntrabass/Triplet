2b11ad757a11312980d815c55755c3046e0a8a4c
Vincent ryu <bigasiler@gmail.com>
2026-09-14 22:58:51 +0900

    아이템 팝업 정리

--- Assets/_Project/Scripts/Gameplay/PlayerController.cs
+++ Assets/_Project/Scripts/Gameplay/PlayerController.cs
@@ -54,25 +54,6 @@ public class PlayerController : MonoBehaviour
         TilesMatched?.Invoke(typeId, count);
     }
 
-    //private void HandlePickBackSelected(ItemState state, TileView tile)
-    //{
-    //    if (_trayController.TryAdd(tile) == false)
-    //    {
-    //        Debug.Log("수납함이 가득 찼습니다.");
-    //        return;
-    //    }
-
-    //    if (_boardController.TryDetachTile(tile) == false)
-    //    {
-    //        throw new InvalidOperationException("선택한 타일을 보드에서 분리하지 못했습니다.");
-    //    }
-
-    //    if (state.TryConsume() == false)
-    //    {
-    //        throw new InvalidOperationException("PickBack 아이템을 소모하지 못했습니다.");
-    //    }
-    //}
-
     public IReadOnlyList<TileView> GetWarehouseCandidates()
     {
         var candidates = new List<TileView>();
@@ -83,18 +64,6 @@ public class PlayerController : MonoBehaviour
 
         return candidates;
     }
-
-    //private bool TryBeginPickBack(ItemState state)
-    //{
-    //    if (_trayController.IsFull)
-    //    {
-    //        return false;
-    //    }
-
-    //    IReadOnlyList<TileView> candidates = _boardController.GetAllTiles();
-
-    //    return _tileSelectionController.TryBegin(candidates, "가져올 타일을 선택하세요", x => HandlePickBackSelected(state, x));
-    //}
 
     private bool TryUseHammer()
     {
